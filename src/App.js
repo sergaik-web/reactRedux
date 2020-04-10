c40226b94c005943fc7f66b800e2f55ac4be737a
sergaik-web <seroga8863@gmail.com>
2020-04-10 14:58:18 +0200

    Create ReactApp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+let items = [
+  "1 lb Salmon",
+  "1 cup Pine Nuts",
+  "2 cups Butter Lettuce",
+  "1 Yellow Squash",
+  "1/2 cup Olive Oil",
+  "3 cloves of Garlic",
+];
+
+let elem = React.createElement(
+  "ul",
+  { className: "ingredients" },
+  items.map((ingredient) => React.createElement("li", null, ingredient))
+);
 
 function App() {
   return (
@@ -8,6 +23,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {elem}
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
